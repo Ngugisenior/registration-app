54ddf7d824fd8f528016a469343cbb39f25c2c63
Ngugisenior <ngugijoseph992@gmail.com>
2021-02-16 09:09:30 +0100

    login

---
+++ src/main/java/com/example/LoginApp/registration/EmailValidator.java
@@ -0,0 +1,11 @@
+package com.example.LoginApp.registration;
+
+import java.util.function.Predicate;
+
+public class EmailValidator implements Predicate<String> {
+    @Override
+    public boolean test(String s) {
+        // TODO: Regex to validate
+        return true;
+    }
+}
