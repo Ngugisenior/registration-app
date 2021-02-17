e3ccf3caeb3396733afb087406c554dc3d083f68
Ngugisenior <ngugijoseph992@gmail.com>
2021-02-17 05:46:08 +0100

    registration token generator

--- src/main/java/com/example/LoginApp/appuser/AppUserService.java
+++ src/main/java/com/example/LoginApp/appuser/AppUserService.java
@@ -1,10 +1,16 @@
 package com.example.LoginApp.appuser;
 
+import com.example.LoginApp.registration.token.ConfirmationToken;
+import com.example.LoginApp.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -12,6 +18,9 @@ public class AppUserService implements UserDetailsService {
 
     private final static String USER_NOT_FOUND_MSG = "user with email %s not found";
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
@@ -19,7 +28,29 @@ public class AppUserService implements UserDetailsService {
     }
 
     public String signUpUser(AppUser appUser){
-        // Todo: Sign Up User
-        return "";
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+        if(userExists){
+            throw new IllegalStateException("email already taken");
+        }
+
+        String encodedPassword = passwordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        String token = UUID.randomUUID().toString();
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        // TODO: Send Email
+        return token;
     }
 }
